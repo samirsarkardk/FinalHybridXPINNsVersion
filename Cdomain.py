4509bc5ddaa6a07234b752ed5c61df80053977f5
samirsarkardk <samirsarkar411411@gmail.com>
2026-09-25 03:43:45 +0530

    modify domain.py

--- Cdomain.py
+++ Cdomain.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 config = Config()
-delta = config.delta
 
 
 x_min = config.x_min
@@ -19,21 +18,21 @@ t_mid = (t_max + t_min)/2
 
 t_mid.to(DEVICE)
 
-def subdomain1(delta):
+def subdomain1():
 
     x1_min = x_min
     x1_max = x_max
 
     t1_min = t_min
-    t1_max = t_mid - delta
+    t1_max = t_mid 
 
     return x1_min, x1_max, t1_min, t1_max
 
 
-def subdomain2(delta):
+def subdomain2():
     x2_min = x_min
     x2_max = x_max
-    t2_min = t_mid + delta
+    t2_min = t_mid 
     t2_max = t_max
 
     return x2_min, x2_max, t2_min, t2_max
@@ -42,52 +41,26 @@ def subdomain2(delta):
 
 
 
-def subdomain3(delta, N=2000):
+def subdomain3():
 
     # Generate candidate points
-    x = torch.rand(N * 10, 1)
-    t = torch.rand(N * 10, 1)
+    x = torch.linspace(x_min,x_max,steps=1000,device=DEVICE).reshape(-1,1)
+    t = torch.full_like(x,t_mid,device=DEVICE)
 
-    # Points in the four corner subdomains
-    mask = ~(
-        (
-            (x <= x_max) &
-            (t <= t_mid - delta)
-        )
-        |
-        (
-            (x <= x_max ) &
-            (t >= t_mid + delta)
-        )
-       
-    )
-
-    x3 = x[mask]
-    t3 = t[mask]
-
-    return x3[:N], t3[:N]
-
-
-
-# x5, t5 = subdomain5(delta, N=2000)
-
-# print(x5.shape)
-# print(t5.shape)
+    return x,t
 
 
 
 
-# N = 2000
-
-# x1_min, x1_max, t1_min, t1_max = subdomain1(delta)
-
-# x1 = x1_min + (x1_max - x1_min) * torch.rand(N,1)
-# t1 = t1_min + (t1_max - t1_min) * torch.rand(N,1)
-# X = torch.cat((x1,t1), dim=1)
 
 
-# print(x1.shape)
-# print(t1.shape)
-# print(X.shape)
+
+
+
+
+
+
+
+
 
 
